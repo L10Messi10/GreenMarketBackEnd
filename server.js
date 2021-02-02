f432429ea0e0160aca95ea4dd9881625592c7046
Greg Segotier <49228795+L10Messi10@users.noreply.github.com>
2021-02-02 16:43:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,7 @@ mobileApp.tables.add('V_Categories_Display');
 mobileApp.tables.add('TBL_Feedback');
 mobileApp.tables.add('TBL_SystemUsers');
 mobileApp.tables.add('TBL_Addresses');
+mobileApp.tables.add('TBL_Riders');
+mobileApp.tables.add('TBL_Delivery');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
